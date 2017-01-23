9801cd7bb15663d26172f48e69e2caec00bd5e85
lroquediniz <lroque.diniz@gmail.com>
2017-01-23 20:05:04 -0200

    Arquivo README de informações do projeto

--- source/src/main/resources/import.sql
+++ source/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-insert into Pessoa (nome, produtividade) values ('João', 10);
+insert into Pessoa (nome, produtividade) values ('Joao', 10);
 insert into Pessoa (nome, produtividade) values ('Lucas', 29);
 insert into Pessoa (nome, produtividade) values ('Pedro', 25);
 insert into Pessoa (nome, produtividade) values ('Lucia', 25);
 
-insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 1', 1, 1, 0, 1);
---insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 2', 50, 2, 0, 2130);
---insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 3', 60, 3, 0, 2700);
---insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 4', 30, 4, 0, 1350);
+insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 1', 1, 1, 0, 54);
+insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 2', 50, 2, 0, 2130);
+insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 3', 60, 3, 0, 2700);
+insert into Atividade (descricao, esforco, pessoa_id, status, tempoExecucao) values ( 'Atividade 4', 30, 4, 0, 1350);
